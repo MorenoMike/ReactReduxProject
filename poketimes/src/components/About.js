5812fcbb3ce2426a9eac788b76cbea6beab09b24
Michael Moreno <morenomike100@gmail.com>
2018-12-15 12:44:58 -0800

    working on componets for poketiems project

---
+++ poketimes/src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Rainbow from '../hoc/Rainbow';
+
+const About = () =>{
+    return (
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi necessitatibus ducimus nihil. Similique aliquid magni ut iure! Assumenda consequuntur aperiam atque maiores doloribus, vitae a quia quidem expedita nisi sit.</p>
+        </div>
+    )
+}
+export default Rainbow(About);
---
+++ poketimes/src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = (props) =>{
+   
+    return (
+        <div className="container">
+            <h4 className="center">Contact</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi necessitatibus ducimus nihil. Similique aliquid magni ut iure! Assumenda consequuntur aperiam atque maiores doloribus, vitae a quia quidem expedita nisi sit.</p>
+        </div>
+    )
+}
+export default Contact;
