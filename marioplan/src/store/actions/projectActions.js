6815151e8fe22e48e6af635fc7190b036ae31636
Michael Moreno <morenomike100@gmail.com>
2019-01-15 15:38:46 -0800

    Commiting changes

--- marioplan/src/store/actions/projectActions.js
+++ marioplan/src/store/actions/projectActions.js
@@ -1,6 +1,17 @@
 export const createProject = (project) => {
-    return (dispatch, getState) => {
+    return (dispatch, getState, {getFirebase, getFirestore}) => {
         //make async call to database
-        dispatch({type: 'CREATE-PROJECT', project});
+        const firestore = getFirestore();
+        firestore.collection('projects').add({
+            ...project,
+            authorFirstName: 'Michael',
+            authorLastName: 'Moreno',
+            authorId: 1234,
+            createdAt: new Date()
+        }).then(() => {
+            dispatch({type: 'CREATE-PROJECT', project});
+        }).catch((err) => {
+            dispatch({type: 'CREATE_PROJECT_ERROR', err});
+        })
     }
 };
