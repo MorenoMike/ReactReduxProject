e24b331b8da03614764023bdf7073c9a8cb76aaf
Michael Moreno <morenomike100@gmail.com>
2019-01-27 16:43:15 -0800

    auth creating

--- marioplan/src/components/layout/Navbar.js
+++ marioplan/src/components/layout/Navbar.js
@@ -4,20 +4,20 @@ import SignedInLinks from './SignedInLinks'
 import SignedOutLinks from './SignedOutLinks'
 import {connect} from 'react-redux'
 
-const Navbar = () => {
+const Navbar = (props) => {
+    const {auth} = props;
+    const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks/>
     return (
         <nav className="nav-wrapper red darken-3">
             <div className="container">
                 <Link to='/' className="brand-logo">Mike's Plan</Link>
-                <SignedInLinks />
-                <SignedOutLinks />
+                {links}
             </div>
         </nav>
     )
 }
 
 const mapStateToProps = (state) =>{
-    console.log(state);
     return{
         auth: state.firebase.auth
     }
--- marioplan/src/components/projects/ProjectDetails.js
+++ marioplan/src/components/projects/ProjectDetails.js
@@ -1,49 +1,52 @@
 import React from 'react'
-import {connect} from 'react-redux';
-import {firestoreConnect} from 'react-redux-firebase'
-import {compose} from 'redux'
+import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase'
+import { compose } from 'redux'
+import { Redirect } from 'react-router-dom'
 
-const ProjectDetails=(props)=> {
-    const {project} = props;
+const ProjectDetails = (props) => {
+    const { project, auth } = props;
+    if (!auth.uid) return <Redirect to='/signin' />
     if (project) {
-        return(
-         <div className="container section project-details">
-          <div className="card z-depth-0">
-              <div className="card-content">
-                  <span className="card-title">{project.title}</span>
-                  <p>{project.content}</p>
-                  <div className="card-action grey lighten-4 grey-text">
-                      <div>Posted by {project.authorFirstName} {project.authorLastName} </div>
-                      <div>12/27/2018</div>
-                  </div>
-              </div>
-          </div>
-      </div>
+        return (
+            <div className="container section project-details">
+                <div className="card z-depth-0">
+                    <div className="card-content">
+                        <span className="card-title">{project.title}</span>
+                        <p>{project.content}</p>
+                        <div className="card-action grey lighten-4 grey-text">
+                            <div>Posted by {project.authorFirstName} {project.authorLastName} </div>
+                            <div>12/27/2018</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         )
     } else {
         return (
             <div className="container center">
                 <p>Loading project...</p>
             </div>
-            )
+        )
     }
-  
+
 }
 
 const mapStateToProps = (state, ownProps) => {
-   // console.log(state);
+    // console.log(state);
     const id = ownProps.match.params.id;
     const projects = state.firestore.data.projects;
     const project = projects ? projects[id] : null
-    return{
-        project: project
+    return {
+        project: project,
+        auth: state.firebase.auth
     }
 }
 
 export default compose(
     connect(mapStateToProps),
     firestoreConnect([
-        {collection: 'projects'}
+        { collection: 'projects' }
     ])
 )(ProjectDetails)
 
